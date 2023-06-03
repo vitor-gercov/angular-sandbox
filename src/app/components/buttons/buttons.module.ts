25e84258a99a0d83fa64a438b8de3a2a25d80d2a
Vitor Gobato Gercov <gercovvitor@gmail.com>
2023-06-03 10:15:37 -0300

    evolucao na tela de gerador de formularios

--- src/app/components/buttons/buttons.module.ts
+++ src/app/components/buttons/buttons.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from './button/button.component';
 import { StrokedButtonComponent } from './stroked-button/stroked-button.component';
+import { IconButtonComponent } from './icon-button/icon-button.component';
 
 
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    StrokedButtonComponent
+    StrokedButtonComponent,
+    IconButtonComponent
   ],
   imports: [
     CommonModule,
     MatButtonModule
   ],
-  exports: [ButtonComponent, StrokedButtonComponent]
+  exports: [ButtonComponent, StrokedButtonComponent, IconButtonComponent]
 })
 export class ButtonsModule { }
